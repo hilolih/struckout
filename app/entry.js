7d043a3fa8092cbe1c37e5e2fe865d8e5b0519c3
hilolih <hilolih@gmail.com>
2018-11-21 18:40:15 +0900

    socket.io sample

--- app/entry.js
+++ app/entry.js
@@ -7,4 +7,12 @@ block1.click(() => {
     block1.animate({width: '100pt', height: '100pt'}, 2000);
 });
 
+import io from 'socket.io-client';
+const socket = io('http://10.255.1.146:8000');
+
+const status = $('#status');
+socket.on('status', (data) => {
+  status.text(data.code.toString());
+});
+
 
