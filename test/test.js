93ae60e145c86a1e16a0d71764d1a7abc668fe4f
hilolih <hilolih@gmail.com>
2018-11-21 18:02:51 +0900

    add test/test.js

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+const assert = require('assert');
+//const dc = require('../');
+
+describe('#test()', () => {
+
+  it('テストのテスト', () => {
+    assert.equal("hello", "hello");
+  });
+
+});
+
+
