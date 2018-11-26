e23660595e547d443469512bf137319c00b69561
hilolih <hilolih@gmail.com>
2018-11-26 12:44:41 +0900

    jsonCheck関数ブラウザへemitする前にJSONをチェックする

--- routes/panel.js
+++ routes/panel.js
@@ -4,19 +4,25 @@ var io = require('socket.io-client');
 var socket = io('http://10.255.1.146:8000');
 
 function jsonCheck(json){
-    if ( typeof(data.no) === 'undefined' ) {
+    if ( typeof(json.no) === 'undefined' ) {
         return false
     }
-    if ( typeof(data.color) !== 'undefined' ) {
+    if ( typeof(json.color) === 'undefined' ) {
         return false
     }
     return true
 }
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    socket.emit('status', req.body);
-    res.send("OK.");
+    if (jsonCheck(req.body)) {
+        console.log(req.body);
+        socket.emit('status', req.body);
+        res.send("OK.");
+    } else {
+        console.log('[!] ERROR: invalid JSON');
+        console.log(req.body);
+        res.send("ERROR.");
+    }
 });
 
 module.exports = router;
